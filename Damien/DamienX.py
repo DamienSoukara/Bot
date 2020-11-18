ae981708f0e56206dd142aaf5896ec5437d9e0b9
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-11-18 03:29:34 +0100

    Update DamienX.py

--- Damien/DamienX.py
+++ Damien/DamienX.py
@@ -19,6 +19,17 @@ def _about(client, message):
     photoUrl = "https://telegra.ph/file/aa59c3024666f7bc9f712.jpg"
     client.send_photo(chatID, photoUrl)
 
+@client.on_message(filters.private & filters.command('test'))
+def _test(client, message):
+    chatID = message.chat.id # ايدي المحادثة
+    photolink = "https://i.imgur.com/5Fw6nMR.jpg" # ايدي الصورة
+    # الوصف اسفل الصورة بتنسيق ماركداون ليظهر بشكل مائل
+    caption = "__DAMIEN__" 
+    style = "Mardown" # نوع التنسيق يمكنك الاختيار بين Markdown او HTML
+    # تنسيق ماركداون : https://core.telegram.org/bots/api#markdown-style
+    # تنسيق HTML : https://core.telegram.org/bots/api#html-style
+    client.send_photo(chatID, photolink, caption, style)
+
 @Client.on_message(filters.private & filters.incoming & filters.command(['help']))
 def _help(client, message):
     client.send_message(chat_id = message.chat.id,
