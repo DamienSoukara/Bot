aa0909d390944f2ab1eb9d0b2fff204958fcd0d3
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-11-18 13:07:14 +0100

    Update DamienX.py

--- Damien/DamienX.py
+++ Damien/DamienX.py
@@ -17,14 +17,12 @@ def _about(client, message):
     chatID = message.chat.id
     photoUrl = "https://telegra.ph/file/aa59c3024666f7bc9f712.jpg"
     client.send_photo(chatID, photoUrl)
-    reply_markup=InlineKeyboardMarkup(
+    reply_markup = InlineKeyboardMarkup(
                     [[InlineKeyboardButton("👑 My Owner", url=f"t.me/AmineSoukara"),
                     InlineKeyboardButton("💬 Channel", url="t.me/DamienSoukara")],
                     [InlineKeyboardButton("🔥 Deploy DAMIEN-X", url="https://heroku.com/deploy?template=https://github.com/AmineSoukara/Damien-X/tree/alpha")
                     ]]
                 )
-            )
-
 
 @Client.on_message(filters.private & filters.command('test'))
 def _test(client, message):
