5f1b42aaf0bd82ce7aecaae38120b95a141c1fd2
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-11-18 15:47:12 +0100

    Update DamienConfig.py

--- DamienConfig.py
+++ DamienConfig.py
@@ -7,11 +7,13 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
     START = os.environ.get("START", "Hi youuu")
+    RULES = os.environ.get("RULES", "MKYN RULESZ)
   else:
     BOT_TOKEN = "1236676824:AAHBjEYEf5M5jk5rfrZ7qShepqNS4nFbR5s"
     APP_ID = "1358970"
     API_HASH = "57aff1848504fcde424d181d5cfee983"
     START = "HI BABY"
+    RULES = "NOTHINGAZBI"
 
 class Messages():
       HELP_MSG = [
