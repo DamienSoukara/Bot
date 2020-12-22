7953cb651f20a1b708aeb047d8f0d739a343a72e
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-22 15:54:43 +0100

    Update commands.py

--- Damien/commands.py
+++ Damien/commands.py
@@ -63,34 +63,3 @@ def _help(c, message):
     )
 
 help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
-
-@Client.on_callback_query(help_callback_filter)
-def help_answer(c, callback_query):
-    chat_id = callback_query.from_user.id
-    message_id = callback_query.message.message_id
-    msg = int(callback_query.data.split('+')[1])
-    c.edit_message_text(chat_id=chat_id,    message_id=message_id,
-        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
-    )
-
-
-def map(pos):
-    if(pos==1):
-        button = [
-            [InlineKeyboardButton(text = '➡️', callback_data = "help+2")]
-        ]
-    elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://t.me/damienhelp"
-        button = [
-            [InlineKeyboardButton(text = '🔔 Updates Channel 🔔', url="https://t.me/DamienSoukara")],
-            [InlineKeyboardButton(text = '📣 Support Chat 📣', url=url)],
-            [InlineKeyboardButton(text = '⬅️', callback_data = f"help+{pos-1}")]
-        ]
-    else:
-        button = [
-            [
-                InlineKeyboardButton(text = '⬅️', callback_data = f"help+{pos-1}"),
-                InlineKeyboardButton(text = '➡️', callback_data = f"help+{pos+1}")
-            ],
-        ]
-    return button
