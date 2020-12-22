0d67d2ffa89a7f7957ab49371dd1fcf899dc7a33
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-22 03:40:10 +0100

    Update DamienX.py

--- Damien/DamienX.py
+++ Damien/DamienX.py
@@ -94,34 +94,3 @@ def map(pos):
             ],
         ]
     return button
-@Client.on_callback_query()
-async def cb_handler(c, m):
-  cb_data = m.data
-
-  if "feed" in cb_data:
-      Config.feedback.append(m.from_user.id)
-      button = [[InlineKeyboardButton("❌ Cancel", callback_data="cancel")]]
-      markup = InlineKeyboardMarkup(button)
-      await m.message.delete()
-      await c.send_message(chat_id=m.message.chat.id, text="💬 Send Your FeedBack Here I Will Notify The Admin.", reply_markup=markup)
-
-  if "cancel" in cb_data:
-      if m.from_user.id in Config.feedback:
-         Config.feedback.remove(m.from_user.id)
-      if m.from_user.id in Config.LOGIN:
-         Config.LOGIN.remove(m.from_user.id)
-      await m.message.delete()
-      await start(c, m.message)
-
-  if "rules" in cb_data:
-      await m.message.delete()
-      await c.send_message(chat_id=m.message.chat.id, text=Translation.RULES)
-
-  if "reply" in cb_data:
-      id = m.data.split("+")[1]
-      Config.SEND.append(id)
-      await c.send_message(chat_id=m.message.chat.id, text="💬 Reply Me, The Text Which You Wanted To Send")
-
-  if "about" in cb_data:
-      await m.message.delete()
-      await c.send_message(chat_id=m.message.chat.id, text="ABOUT", disable_web_page_preview=True)
