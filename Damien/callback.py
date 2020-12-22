157fdae1b9da217a314ad97bfceadca0c148ca35
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-22 15:21:30 +0100

    Update callback.py

--- Damien/callback.py
+++ Damien/callback.py
@@ -4,91 +4,52 @@
 
 import pyrogram
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 from config import Config
 from translation import Translation
 from .commands import start
 
 @Client.on_callback_query()
-async def cb_handler(bot, m):
+async def cb_handler(c, m):
   cb_data = m.data
 
-    if "helpx" in cb_data:
-        button = [[InlineKeyboardButton("🏠 Home", callback_data="home")]]
-        markup = InlineKeyboardMarkup(button)
-        await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
-                           text=Translation.HELP_USER.format(m.from_user.first_name),
-                           disable_web_page_preview=True,
-                           reply_markup=markup)
-
-    if "ytdl" in cb_data:
-        button = [[InlineKeyboardButton("🔙 Back", callback_data="morehelp"),
-                InlineKeyboardButton("🏠 Home", callback_data="home")]]
-        markup = InlineKeyboardMarkup(button)
-        await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
-                           text=Translation.YTDL,
-                           disable_web_page_preview=True,
-                           reply_markup=markup)
-
-    if "urldl" in cb_data:
-        button = [[InlineKeyboardButton("🔙 Back", callback_data="morehelp"),
-                InlineKeyboardButton("🏠 Home", callback_data="home")]]
-        markup = InlineKeyboardMarkup(button)
-        await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
-                           text=Translation.URLDL,
-                           disable_web_page_preview=True,
-                           reply_markup=markup)
-
-    if "renamerx" in cb_data:
-        button = [[InlineKeyboardButton("🔙 Back", callback_data="morehelp"),
-                InlineKeyboardButton("🏠 Home", callback_data="home")]]
-        markup = InlineKeyboardMarkup(button)
-        await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
-                           text=Translation.RENAMERX,
-                           disable_web_page_preview=True,
-                           reply_markup=markup)
-
-    if "morehelp" in cb_data:
-        button = [[
-                InlineKeyboardButton("🌐 Url Upload", callback_data="urldl"),
-                InlineKeyboardButton("✍ Renamer", callback_data="renamerx"),
-                ],
-                [
-                InlineKeyboardButton("🎞 YouTube DL", callback_data="ytdl"),
-                InlineKeyboardButton("🤖 Feedback", url="t.me/DamienRobot")
-                ],
-                [InlineKeyboardButton("🏠 Home", callback_data="home")]]
-        markup = InlineKeyboardMarkup(button)
-        await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
-                           text=Translation.MOREHELP.format(m.from_user.first_name),
-                           reply_markup=markup)
-
-    if "about" in cb_data:
-        button = [[InlineKeyboardButton("🏠 Home", callback_data="home")]]
-        markup = InlineKeyboardMarkup(button)
-        await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
-                           text=Translation.ABOUTT.format(m.from_user.first_name),
-                           disable_web_page_preview=True,
-                           reply_markup=markup)
-
-    if "home" in cb_data:
+  if "home" in cb_data:
       button = [[
                 InlineKeyboardButton("💬 Updates Channel", url="t.me/DamienSoukara"),
                 InlineKeyboardButton("🗣 Support Group", url="t.me/damienhelp"),
                 ],
                 [
                 InlineKeyboardButton("ℹ About", callback_data="about"),
-                InlineKeyboardButton("🤔 Help", callback_data="morehelp")
+                InlineKeyboardButton("🤔 Help", callback_data="helpx")
                 ],
                 [InlineKeyboardButton("🤴 Developer 🤴", url="t.me/AmineSoukara")]]
       markup = InlineKeyboardMarkup(button)
-      await bot.edit_message_text(chat_id=m.message.chat.id,
-                           message_id=m.message.message_id,
+      await m.message.delete()
+      await c.send_message(chat_id=m.message.chat.id,
                            text=Translation.START_MSG.format(m.from_user.first_name),
+                           reply_markup=markup)
+
+  if "feed" in cb_data:
+      Config.Client.append(m.from_user.id)
+      button = [[InlineKeyboardButton("❌ Cancel", callback_data="cancel")]]
+      markup = InlineKeyboardMarkup(button)
+      await m.message.delete()
+      await c.send_message(chat_id=m.message.chat.id, text="💬 Send Your FeedBack Here I Will Notify The Admin.", reply_markup=markup)
+
+  if "helpx" in cb_data:
+      button = [[InlineKeyboardButton("🏠 Home", callback_data="home")]]
+      markup = InlineKeyboardMarkup(button)
+      await m.message.delete()
+      await c.send_message(chat_id=m.message.chat.id,
+                           text=Translation.HELP_USER.format(m.from_user.first_name),
+                           disable_web_page_preview=True,
+                           reply_markup=markup)
+
+  if "about" in cb_data:
+      button = [[InlineKeyboardButton("🏠 Home", callback_data="home")]]
+      markup = InlineKeyboardMarkup(button)
+      await m.message.delete()
+      await c.send_message(chat_id=m.message.chat.id,
+                           text=Translation.ABOUT.format(m.from_user.first_name),
+                           disable_web_page_preview=True,
                            reply_markup=markup)
