f094b8b049c2b9c662a7f625884c8e0a5ca8c94c
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 15:12:19 +0100

    Create belp.py

---
+++ belp.py
@@ -0,0 +1,8 @@
+import os
+
+class Messages():
+    HELP_MSG = [
+        ".",
+        "**Hello 🌍 World**",
+        "Another Text 😅",
+        "**[👨‍💻](https://i.imgur.com/TaOKIkf.gif) Developed By : @AmineSoukara**"
