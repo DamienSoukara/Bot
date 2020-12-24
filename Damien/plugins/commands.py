15bf00b263484fc7261d8343a14dfb906e377909
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 19:54:11 +0100

    Update commands.py

--- Damien/plugins/commands.py
+++ Damien/plugins/commands.py
@@ -2,26 +2,39 @@ import logging
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 
 from Damien import bot
 from help import Messages as tr
 from translation import Translation
-
+from helper_funcs.chat_base import TRChatBase
 logging.basicConfig(level=logging.INFO)
-
-
-@bot.on_message(filters.private & filters.incoming & filters.command(["startt"]))
-def _startt(c, m):
-    c.send_message(
-        m.chat.id,
-        text=tr.X_MSG.format(m.from_user.first_name, m.from_user.id),
-        parse_mode="markdown",
-        reply_to_message_id=m.message_id,
-    )
-
 
 @bot.on_message(filters.command(["start"]))
 async def start(c, m):
+    if m.from_user.id in Config.BANNED_USERS:
+        await m.reply_text("You are B A N N E D 🤣🤣🤣🤣")
+        return
+    TRChatBase(m.from_user.id, m.text, "start")
+    update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await c.get_chat_member(update_channel, m.chat.id)
+            if user.status == "kicked":
+               await m.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+               return
+        except UserNotParticipant:
+            #await m.reply_text(f"Join @{update_channel} To Use Me")
+            await m.reply_text(
+                text="[●](https://i.imgur.com/t1JsZ0I.gif) **Join My Updates Channel To Mse Me : **",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Subscribe", url=f"https://t.me/{update_channel}")]
+              ])
+            )
+            return
+        except Exception:
+            await m.reply_text("Something Wrong. Contact @AmineSoukara")
+            return
     chatID = m.chat.id
     photoUrl = "https://i.imgur.com/x2igrLQ.jpg"
     button = [
