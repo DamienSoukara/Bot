50d1452c667b62434b3b07c9ccc044cd2c4bd102
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 18:35:04 +0000

    DAMIEN : ⚙ Fixes

--- helper_funcs/chat_base.py
+++ helper_funcs/chat_base.py
@@ -3,23 +3,20 @@
 # ©️ @AmineSoukara.
 
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-from config import Config
-from translation import Translation
-from pyrogram import Client, filters
 from chatbase import Message
+from config import Config
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def TRChatBase(chat_id, message_text, intent):
     msg = Message(api_key=Config.CHAT_BASE_TOKEN,
-              platform="Telegram",
-              version="1.3",
-              user_id=chat_id,
-              message=message_text,
-              intent=intent)
-    resp = msg.send()
+                  platform="Telegram",
+                  version="1.3",
+                  user_id=chat_id,
+                  message=message_text,
+                  intent=intent)
+    msg.send()
