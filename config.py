da30c2281eacdb681d241e5ffe3e892ee0cdafe1
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 02:08:32 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,31 @@
 import os
 
-class Config():
-  ENV = bool(os.environ.get('ENV', False))
-  if ENV:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    APP_ID = os.environ.get("APP_ID", 6)
-    API_HASH = os.environ.get("API_HASH", None)
-    START = os.environ.get("START", "Hi youuu")
-    RULES = os.environ.get("RULES", "MKYN RULESZ")
-  else:
-    BOT_TOKEN = "1495791391:AAGzm8Dh3IHoG7xtZbNezoINLxverPfRktQ"
-    APP_ID = "1250763"
-    API_HASH = "11d05c637abdb46e35f51bc73241c75c"
-    START = "HI BABY"
-    RULES = "NOTHINGAZBI"
-    AUTH_CHATS = set([-1001481357570])  # @UserGeOt
+from dotenv import load_dotenv
+
+if os.path.isfile("config.env"):
+    load_dotenv("config.env")
+
+
+class Config:
+    """ assistant configs """
+    APP_ID = int(os.environ.get("APP_ID", 0))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    AUTH_CHATS = set([-1001474594528])  # @UserGeOt
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
     WHITELIST_CHATS = set([-1001465749479])  # @UserGeSpam
     if os.environ.get("WHITELIST_CHATS"):
         WHITELIST_CHATS.update(map(int, os.environ.get("WHITELIST_CHATS").split()))
     DEV_USERS = (
-        853393439,  # @X
+        853393439,  # @me
         1110621941,  # @PhycoNinja13b
         921420874,   # @juznem
         837784353    # @rking_32
     )
     ADMINS = {}
     MAX_MSG_LENGTH = 4096
+
 
 class Messages():
       HELP_MSG = [
