d8463204cc16b1c4af555693a6a5da13f88e7d47
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 03:22:27 +0100

    Create bot.py

---
+++ Damien/bot.py
@@ -0,0 +1,21 @@
+__all__ = ["bot", "START_TIME"]
+
+import time
+
+from pyrogram import Client
+
+from . import Config, logging
+
+_LOG = logging.getLogger(__name__)
+START_TIME = time.time()
+
+plugins = dict(root="Damien")
+bot = Client(
+     'Damien Soukara',
+      bot_token = Config.BOT_TOKEN,
+      api_id = Config.APP_ID,
+      api_hash = Config.API_HASH,
+      plugins = plugins
+)
+
+_LOG.info("assistant-bot initialized!")
