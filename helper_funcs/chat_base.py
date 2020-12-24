5bc2ce63a3179deaad54c7e1fa96cad6cfcbbeaa
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 19:34:32 +0100

    Update chat_base.py

--- helper_funcs/chat_base.py
+++ helper_funcs/chat_base.py
@@ -2,33 +2,24 @@
 # -*- coding: utf-8 -*-
 # ©️ @AmineSoukara.
 
-# the logging things
-from chatbase import Message
-import os
+
 import logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
-# the Strings used for this "thing"
-
-
-# the Telegram trackings
+import os
+from config import Config
+from translation import Translation
+from pyrogram import Client, filters
+from chatbase import Message
 
 
 def TRChatBase(chat_id, message_text, intent):
     msg = Message(api_key=Config.CHAT_BASE_TOKEN,
-                  platform="Telegram",
-                  version="1.3",
-                  user_id=chat_id,
-                  message=message_text,
-                  intent=intent)
-    msg.send()
+              platform="Telegram",
+              version="1.3",
+              user_id=chat_id,
+              message=message_text,
+              intent=intent)
+    resp = msg.send()
