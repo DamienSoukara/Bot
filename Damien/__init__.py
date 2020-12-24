7d4abb649be9d0c935adcf8a5e22ea3051cab96e
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-24 01:56:23 +0100

    Update __init__.py

--- Damien/__init__.py
+++ Damien/__init__.py
@@ -1,7 +1,4 @@
 # Copyright 
 
-from .logger import logging  # noqa
 from .config import Config  # noqa
-from .bot import bot  # noqa
 from . import cus_filters  # noqa
-from .database import DB, save_data, load_data  # noqa
