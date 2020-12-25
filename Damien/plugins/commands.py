cd3c8d3e7e7d5a703e587bb301dd7fa45062c705
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-25 14:02:09 +0000

    DAMIEN : ⚙ Fixes

--- Damien/plugins/commands.py
+++ Damien/plugins/commands.py
@@ -1,13 +1,10 @@
 import logging
 
 from pyrogram import filters
-from pyrogram.errors import UserNotParticipant
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from config import Config
 from Damien import bot
 from help import Messages as tr
-from helper_funcs.chat_base import TRChatBase
 from translation import Translation
 
 logging.basicConfig(level=logging.INFO)
