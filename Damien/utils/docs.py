c5184a78ccb6cfe4998f297b3d21ce36695dbdc4
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-25 23:37:55 +0100

    Update docs.py

--- Damien/utils/docs.py
+++ Damien/utils/docs.py
@@ -1,24 +1,3 @@
-#  MIT License
-#
-#  Copyright (c) 2019-2020 Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
 
 import re
 
@@ -289,6 +268,7 @@ ABOUT_BOT_THUMB = "https://i.imgur.com/zRglRz3.png"
 OPEN_BOOK_THUMB = "https://i.imgur.com/v1XSJ1D.png"
 RED_HEART_THUMB = "https://i.imgur.com/66FVFXz.png"
 SCROLL_THUMB = "https://i.imgur.com/L1u0VlX.png"
+BANNED = "https://i.imgur.com/zDbjjQw.jpg"
 
 HELP = (
     f"{emoji.ROBOT} **Pyrogram Assistant**\n\n"
@@ -305,6 +285,11 @@ HELP = (
     f"`@pyrogrambot !d` – Decorators\n"
     f"`@pyrogrambot !rm` – Raw Methods\n"
     f"`@pyrogrambot !rt` – Raw Types\n\n")
+
+BAN = (
+    f"{emoji.ROBOT} **SixPathsOfPain Assistant**\n\n"
+    f"`HAHAHA` – RESPECT RULES BRO\n"
+    f"`Join My Channel` – @DamienSoukara\n\n")
 
 DEFAULT_RESULTS = [
     InlineQueryResultArticle(
@@ -396,4 +381,23 @@ RULES = [
         thumb_url=SCROLL_THUMB,
     )
     for i, rule in enumerate(rules.split("\n")[3:-3])
+]
+
+BAN_RESULTS = [
+    InlineQueryResultArticle(
+        title="You're Banned",
+        input_message_content=InputTextMessageContent(BAN, disable_web_page_preview=True, parse_mode="markdown"),
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton(
+                f"{emoji.FIRE} Feedback ",
+                url="t.me/DamienRobot"
+            ),
+            InlineKeyboardButton(
+                f"{emoji.FIRE} Go!",
+                switch_inline_query=""
+            )
+        ]]),
+        description="Do You Wanna Know Why ?",
+        thumb_url=BANNED,
+    ),
 ]
