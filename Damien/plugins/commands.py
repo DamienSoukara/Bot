8138e5be6254baeb0c236c5799b3a52355193b15
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-25 15:01:42 +0100

    Update commands.py

--- Damien/plugins/commands.py
+++ Damien/plugins/commands.py
@@ -13,38 +13,6 @@ from translation import Translation
 logging.basicConfig(level=logging.INFO)
 
 
-@bot.on_message(filters.private)
-async def sub(c, m: Message):
-    if m.from_user.id in Config.BANNED_USERS:
-        await m.reply_text("You are B A N N E D 🤣🤣🤣🤣 #Dev")
-        return
-    TRChatBase(m.from_user.id, m.text, "start")
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await c.get_chat_member(update_channel, m.chat.id)
-            if user.status == "kicked":
-                await m.reply_text("🤭 Sorry Dude, You are **B A N N E D** #Channel")
-                return
-        except UserNotParticipant:
-            # await m.reply_text(f"Join @{update_channel} To Use Me")
-            await m.reply_text(
-                text="[●](https://i.imgur.com/t1JsZ0I.gif) **Join My Updates Channel To Mse Me : **",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="Subscribe", url=f"https://t.me/{update_channel}"
-                            )
-                        ]
-                    ]
-                ),
-            )
-            return
-        except Exception:
-            await m.reply_text("Something Wrong. Contact @AmineSoukara")
-
-
 @bot.on_message(filters.command(["start"]))
 async def start(c, m):
     chatID = m.chat.id
