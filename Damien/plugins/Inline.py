1d1b57b43bf33ef8af0ebb63e4650c486252a1f5
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-26 01:45:08 +0100

    Update Inline.py

--- Damien/plugins/Inline.py
+++ Damien/plugins/Inline.py
@@ -42,7 +42,9 @@ async def inline(_, query: InlineQuery):
     offset = int(query.offset or 0)
     switch_pm_text = f"{emoji.OPEN_BOOK} Pyrogram Docs"
 
-    if string == "!m":
+    if query.from_user.id in Config.BANNED_USERS:
+        pass
+    if string == "m":
         switch_pm_text = f"{emoji.CLOSED_BOOK} Pyrogram Methods ({len(docs.METHODS)})"
 
         if offset == 0:
